cfd013e5463c46fe496bd4dbd43558fefca54212
Filipe Valente Gomes <filipego@Filipes-MBP.fios-router.home>
2019-09-03 19:01:26 -0400

    added Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+
 import "./App.css";
 
-import HomePage from "./pages/homepage.component";
+import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/Hats" component={HatsPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
