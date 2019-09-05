e54d81176046cd3989870d8d66b4bfe6f6c8e108
Filipe Valente Gomes <filipego@Filipes-MBP.fios-router.home>
2019-09-04 20:12:28 -0400

    replaced files to ficx router bug

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React from "react";
-import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-import "./App.css";
+import './App.css';
 
-import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from "./pages/shop/shop.component";
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 function App() {
   return (
     <div>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/shop" component={ShopPage} />
-        </Switch>
-      </Router>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
